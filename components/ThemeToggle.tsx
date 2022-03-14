dfa7895886d52a071d66dd512ca62c3521b8f1cf
An Xuan Hoang <xuanan2001@gmail.com>
2022-03-14 17:34:31 +0700

    ThemeToggle scale animation

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,13 +1,19 @@
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
+import { animated, config, useTransition } from "react-spring";
 
 const ThemeToggle = () => {
-  const [mounted, setMounted] = useState(false);
   const { setTheme, resolvedTheme } = useTheme();
+  const transition = useTransition<string, CSSProperties>(resolvedTheme, {
+    from: { position: "absolute", transform: "scale(1.5)", opacity: 0 },
+    enter: { transform: "scale(1)", opacity: 1 },
+    leave: { transform: "scale(0.5)", opacity: 0 },
+    config: { ...config.molasses, duration: 300 },
+  });
 
+  const [mounted, setMounted] = useState(false);
   // When mounted on client, now we can show the UI
   useEffect(() => setMounted(true), []);
-
   if (!mounted) return null;
 
   const handleClick = () => {
@@ -15,39 +21,49 @@ const ThemeToggle = () => {
   };
 
   return (
-    <button type="button" onClick={handleClick}>
-      {resolvedTheme === "light" && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-          />
-        </svg>
-      )}
-      {resolvedTheme === "dark" && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-6 h-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          />
-        </svg>
-      )}
+    <button
+      className="relative flex items-center justify-center w-12 h-12"
+      type="button"
+      onClick={handleClick}
+    >
+      {transition((styles, theme) => (
+        <>
+          {theme === "light" && (
+            <animated.svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              style={styles}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              />
+            </animated.svg>
+          )}
+          {theme === "dark" && (
+            <animated.svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              style={styles}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+              />
+            </animated.svg>
+          )}
+        </>
+      ))}
     </button>
   );
 };
